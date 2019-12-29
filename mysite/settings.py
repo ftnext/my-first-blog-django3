3cc18ecc9d5fe73e17594a81e800593194e24606
ftnext <takuyafjp+develop@gmail.com>
2019-12-29 14:10:51 +0900

    [chore] change settings (display in Japanese etc.)

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!v9lm5!om#3%vo3g28#u_+9ghgq4-_y)&_x4t-x=*sno$&7_3%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
